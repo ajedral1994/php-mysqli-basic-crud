81fe6c601cf03f710d531c6d1312bce88137a864
Ruther Bergonia <ruther.bergonia@gmail.com>
2017-09-11 18:58:51 +0800

    added methods for chaining

--- Database.php
+++ Database.php
@@ -8,6 +8,10 @@ class Database
   private $pass;
   private $db;
   private $mysqli;
+  private $selectedFields = array();
+  private $table;
+  private $whereClause;
+  private $limit;
 
 
   public function __construct()
@@ -37,5 +41,70 @@ class Database
     $result = $this->mysqli->query($sql);
     return $result;
   }
+
+  public function select()
+  {
+
+    $this->selectedFields = func_get_args();
+    return $this;
+
+  }
+
+  public function from($table)
+  {
+
+    $this->table = $table;
+    return $this;
+
+  }
+
+  public function where($where)
+  {
+
+    $this->whereClause = $where;
+    return $this;
+
+  }
+
+  public function limit($limit)
+  {
+
+    $this->limit = $limit;
+    return $this;
+  }
+
+  public function result()
+  {
+
+    $query[] = "SELECT";
+    if(empty($this->selectedFields)) {
+      // if fields are empty then select all
+      $query[] = "*";
+    } else {
+      $query[] = join(', ', $this->selectedFields);
+    }
+
+    $query[] = "FROM";
+    $query[] = $this->table;
+
+    if(!empty($this->whereClause)) {
+      $query[] = "WHERE";
+      $query[] = $this->whereClause;
+    }
+
+    if(!empty($this->limit)) {
+      $query[] = "LIMIT";
+      $query[] = $this->limit;
+    }
+
+
+     $query =  join(' ', $query);
+
+     return $this->mysqli->query($query);
+
+  }
+
+
+
 
 }
